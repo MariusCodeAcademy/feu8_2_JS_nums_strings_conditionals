cb868ba041a878af52d819de0a2bda71f1ce3621
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-27 11:02:19 +0300

    log

--- if.js
+++ if.js
@@ -10,7 +10,7 @@ if (salyga) {
 }
 */
 
-let num1 = 500;
+let num1 = 555;
 
 // norim atspausdinti koks yra skaicius
 // skaicius min gali buti 0
@@ -20,6 +20,24 @@ let num1 = 500;
 
 if (num1 <= 10) {
   console.log(`skaicius ${num1} yra nedidelis`);
-} else {
+} else if (num1 <= 60) {
   console.log(`skaicius ${num1} yra vidutinis`);
+} else {
+  console.log(`skaicius ${num1} yra didelis`);
+}
+
+let x = 50;
+// x = 'labas';
+// patikrtinti ar x yra skaicius ir ar didesnti uz 10
+if (typeof x === 'number') {
+  console.log('x yra skaicius');
+  if (x > 10) {
+    console.log('x yra daugiau uz 10');
+  } else {
+    console.log('x nera daugiau uz 10');
+  }
+}
+// Loginiu operatorium patikrtinti ar x yra skaicius IR ar didesnti uz 10
+if (typeof x === 'number' && x > 10) {
+  console.log('x yra skaicius IR daugiau uz 10');
 }
